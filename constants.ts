8475b92cc82c0f81ec997bf56eac0a62a0685fd3
Karthikeya M <kmaddipa@student.gitam.edu>
2025-09-09 15:35:21 +0530

    Create constants.ts

---
+++ constants.ts
@@ -0,0 +1,8 @@
+export const FIXED_CHARGE = 125.00;
+
+export const SLAB_RATES = [
+  { upTo: 100, rate: 3.00 },
+  { upTo: 200, rate: 4.50 },
+  { upTo: 400, rate: 6.50 },
+  { upTo: Infinity, rate: 8.00 },
+];
